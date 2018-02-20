1958e9f2953fe4ecef8b414700d4ba260b1823c5
Wladimir Anselm <Wladimir Anselm@YouTransform.mshome.net>
2018-02-20 19:47:50 +0100

    third

---
+++ Test Suites/Test1.1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test1.1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>65510e76-5bc3-46f8-b1d0-996d0f3868c4</testSuiteGuid>
+</TestSuiteEntity>
