3420251464191089523d44edc4138192632b88a4
Wladimir Anselm <Wladimir Anselm@YouTransform.mshome.net>
2018-02-20 19:30:03 +0100

    second try

---
+++ Object Repository/Page_The worlds leading software de/dd_Email is invalid or already.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>dd_Email is invalid or already</name>
+   <tag></tag>
+   <elementGuidId>84f7e7ec-f175-4788-b71b-b09d2607e6c3</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>dd</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>error</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Email is invalid or already taken</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;logged-out env-production page-responsive min-width-0 f4 intent-mouse&quot;]/div[@class=&quot;application-main&quot;]/div[@class=&quot;jumbotron jumbotron-codelines&quot;]/div[@class=&quot;container-lg p-responsive position-relative&quot;]/div[@class=&quot;d-md-flex flex-items-center gutter-md-spacious&quot;]/div[@class=&quot;mx-auto col-sm-8 col-md-5 hide-sm&quot;]/div[@class=&quot;rounded-1 text-gray bg-gray-light py-4 px-4 px-md-3 px-lg-4&quot;]/form[@class=&quot;home-hero-signup js-signup-form&quot;]/dl[@class=&quot;form-group errored&quot;]/dd[@class=&quot;error&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_The worlds leading software de/h1_Built for developers.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_Built for developers</name>
+   <tag></tag>
+   <elementGuidId>b1db14ed-9272-48e5-a8cf-593851e556d7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>alt-h0 text-white lh-condensed-ultra mb-3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Built for developers</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;logged-out env-production page-responsive min-width-0 f4 intent-mouse&quot;]/div[@class=&quot;application-main&quot;]/div[@class=&quot;jumbotron jumbotron-codelines&quot;]/div[@class=&quot;container-lg p-responsive position-relative&quot;]/div[@class=&quot;d-md-flex flex-items-center gutter-md-spacious&quot;]/div[@class=&quot;col-md-7 text-center text-md-left&quot;]/h1[@class=&quot;alt-h0 text-white lh-condensed-ultra mb-3&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
